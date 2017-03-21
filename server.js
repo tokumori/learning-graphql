fa33beff79acc47602ff3a11f9e9f30f151f8399
Bryce <bsaito88@gmail.com>
2017-03-20 17:25:14 -1000

    Added String, Float, and Int in query.
    - String is determine by math random.
    - Float cannot be null.
    - Int is an array.

--- server.js
+++ server.js
@@ -4,14 +4,22 @@ var { buildSchema } = require('graphql');
 
 var schema = buildSchema(`
   type Query {
-    hello: String
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
   }
 `);
 
 var root = {
-  hello: () => {
-    return 'Hello world!';
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
   },
+  random: () => {
+    return Math.random();
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+  }
 };
 
 var app = express();
